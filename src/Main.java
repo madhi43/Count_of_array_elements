7e77136a57de636b059f1cda8e2475846dd68e8f
madhi43 <103943383+madhi43@users.noreply.github.com>
2023-08-25 13:33:52 +0530

    initial commit done.java

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,28 @@
 import java.util.Scanner;
 public class Main {
-    public static int GreaterElements(int[] A) {
-        int count = 0;
-        int n = A.length;
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] A = new int[n];
+        System.out.println("Enter the elements:");
         for (int i = 0; i < n; i++) {
-            int x = A[i];
-            boolean foundGreater = false;
-
-            for (int j = 0; j < n; j++) {
-                if (A[j] > x) {
-                    foundGreater = true;
-                    break;
-                }
-            }
-
-            if (foundGreater) {
+            A[i] = sc.nextInt();
+        }
+        int result = count(A);
+        System.out.println("Number of elements with at least 1 element greater than itself: " + result);
+    }
+    public static int count(int[] A) {
+        int count = 0;
+        int max = 0;
+        for (int j : A) {
+            max = Math.max(max, j);
+        }
+        for (int j : A) {
+            if (j < max) {
                 count++;
             }
         }
-
         return count;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the number of elements: ");
-
-        int n = scanner.nextInt();
-        int[] A = new int[n];
-
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < n; i++) {
-            A[i] = scanner.nextInt();
-        }
-
-        scanner.close();
-
-        int output = GreaterElements(A);
-        System.out.println("Output: " + output);
     }
 }
